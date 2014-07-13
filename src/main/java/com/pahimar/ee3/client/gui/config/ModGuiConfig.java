b05c425c94586633e3734f890b2851459629b2cd
RIDGEDAMAN <shumate3rd@aol.com>
2014-07-12 23:35:06 -0400

    beginnings of configuration gui

---
+++ src/main/java/com/pahimar/ee3/client/gui/config/ModGuiConfig.java
@@ -0,0 +1,15 @@
+package com.pahimar.ee3.client.gui.config;
+
+import cpw.mods.fml.client.config.GuiConfig;
+import cpw.mods.fml.client.config.IConfigElement;
+import net.minecraft.client.gui.GuiScreen;
+
+import java.util.List;
+
+public class ModGuiConfig extends GuiConfig
+{
+
+    public ModGuiConfig(GuiScreen parentScreen, List<IConfigElement> configElements, String modID, String configID, boolean allRequireWorldRestart, boolean allRequireMcRestart, String title) {
+        super(parentScreen, configElements, modID, configID, allRequireWorldRestart, allRequireMcRestart, title);
+    }
+}
